24244ac01eddd06ba09094369542dc7ea66f2c50
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-12 03:20:33 -0500

    deleting data from angular

--- ClientApp/src/app/components/delete-book/delete-book.component.ts
+++ ClientApp/src/app/components/delete-book/delete-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from 'src/app/services/book.service';
+import Book from 'src/app/interfaces/book';
 
 @Component({
   selector: 'app-delete-book',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: BookService) { }
 
   ngOnInit() {
+    this.service.getBookById(this.route.snapshot.params.id).subscribe(data => {
+      this.book = data;
+    })
   }
 
+  deleteBook(id: number) {
+    this.service.deleteBook(id).subscribe(data => {
+      this.router.navigate(["/books"]);
+    });
+  }
 }

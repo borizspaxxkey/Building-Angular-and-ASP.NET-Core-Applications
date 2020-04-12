7b7abcd49963dc8fb7582412e8153b4deb38bb28
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-04-12 00:42:49 -0500

    books component completed

--- ClientApp/src/app/components/books/books.component.ts
+++ ClientApp/src/app/components/books/books.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Book from 'src/app/interfaces/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-books',
@@ -9,9 +10,13 @@ import Book from 'src/app/interfaces/book';
 export class BooksComponent implements OnInit {
 
   books: Book[];
-  constructor() { }
+
+  constructor(private service: BookService) { }
 
   ngOnInit() {
+    this.service.getAllBooks().subscribe((data) => {
+      this.books = data;
+    });
   }
 
 }
